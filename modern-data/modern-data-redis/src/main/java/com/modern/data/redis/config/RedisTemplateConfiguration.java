94de330490e00c70e9e378c9c031272dbfc61a2e
zhangjun <brucezhang_jjz@163.com>
2024-10-22 15:58:29 +0800

    fix redis

--- modern-data/modern-data-redis/src/main/java/com/modern/data/redis/config/RedisTemplateConfiguration.java
+++ modern-data/modern-data-redis/src/main/java/com/modern/data/redis/config/RedisTemplateConfiguration.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -43,20 +44,15 @@ public class RedisTemplateConfiguration {
         if (pool == null) {
             pool = new RedisProperties.Pool();
         }
-
         GenericObjectPoolConfig<Object> genericObjectPoolConfig = new GenericObjectPoolConfig<>();
         genericObjectPoolConfig.setMaxIdle(pool.getMaxIdle());
         genericObjectPoolConfig.setMaxTotal(pool.getMaxTotal());
-//        genericObjectPoolConfig.setMaxWaitMillis(pool.getMaxWaitMillis());
         genericObjectPoolConfig.setMaxWait(Duration.ofMillis(pool.getMaxWaitMillis()));
-//        genericObjectPoolConfig.setMinEvictableIdleTimeMillis(pool.getMinEvictableIdleTimeMillis());
         genericObjectPoolConfig.setMinEvictableIdleTime(Duration.ofMillis(pool.getMinEvictableIdleTimeMillis()));
         genericObjectPoolConfig.setNumTestsPerEvictionRun(pool.getNumTestsPerEvictionRun());
-//        genericObjectPoolConfig.setTimeBetweenEvictionRunsMillis(pool.getTimeBetweenEvictionRunsMillis());
         genericObjectPoolConfig.setTimeBetweenEvictionRuns(Duration.ofMillis(pool.getTimeBetweenEvictionRunsMillis()));
         genericObjectPoolConfig.setTestOnBorrow(pool.getTestOnBorrow());
         genericObjectPoolConfig.setTestWhileIdle(pool.getTestWhileIdle());
-
         return LettucePoolingClientConfiguration.builder()
                 .commandTimeout(Duration.ofMillis(10_000))
                 .poolConfig(genericObjectPoolConfig)
@@ -67,6 +63,7 @@ public class RedisTemplateConfiguration {
      * 集群模式连接工厂
      */
     @Bean
+    @ConditionalOnSingleCandidate(RedisConnectionFactory.class)
     @ConditionalOnProperty(prefix = "modern.data.redis", name = "type", havingValue = "cluster")
     public RedisConnectionFactory clusterConnectionFactory(LettuceClientConfiguration lettuceClientConfiguration) {
         RedisProperties.Cluster cluster = redisProperties.getCluster();
@@ -83,6 +80,7 @@ public class RedisTemplateConfiguration {
      * 单机模式连接工厂
      */
     @Bean
+    @ConditionalOnSingleCandidate(RedisConnectionFactory.class)
     @ConditionalOnProperty(prefix = "modern.data.redis", name = "type", havingValue = "standalone", matchIfMissing = true)
     public RedisConnectionFactory standaloneConnectionFactory(LettuceClientConfiguration lettuceClientConfiguration) {
         RedisProperties.Standalone standalone = redisProperties.getStandalone();
