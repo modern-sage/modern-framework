64c702ae0423d7182656d93df1f50602944baaa3
zhangjun <brucezhang_jjz@163.com>
2025-05-07 11:40:03 +0800

    c

--- modern-security/modern-security-spring/src/main/java/com/modern/security/spring/filter/AuthenticationTokenFilter.java
+++ modern-security/modern-security-spring/src/main/java/com/modern/security/spring/filter/AuthenticationTokenFilter.java
@@ -39,6 +39,7 @@ public class AuthenticationTokenFilter extends OncePerRequestFilter {
 
     public AuthenticationTokenFilter(AuthenticationDetailsService authenticationDetailsService, String tokenKey) {
         this.authenticationDetailsService = authenticationDetailsService;
+        System.out.println(1);
         this.tokenKey = tokenKey;
     }
 
